478a706af429d6d1944b84e03a2b6197316140e5
Qaziowais13 <24sp-046-se@students.uitu.edu.pk>
2024-12-14 18:37:26 +0500

    Update Person.java

--- src/Person.java
+++ src/Person.java
@@ -2,19 +2,25 @@
 
 public abstract class Person {
     private String name;
-    private int ID;
+    private String ID;
 
-    public Person(String name, int ID) {
+    public Person(String name, String ID) {
         this.name = name;
         this.ID = ID;
     }
 
-    public String get_info() {
-        return "Name: " + name + "\nID: " + ID;
+    public String get_name() {
+        return  name ;
     }
-
+    public String get_id() {
+        return  ID;
+    }
     public void update_info(String name, int ID) {
         this.name = name;
         this.ID = ID;
     }
+    public void displayInfo() {
+   System.out.println("Name:"+ name);
+   System.out.println("ID:"+ ID);
+        
 }
